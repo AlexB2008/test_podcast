bbca9ae55fdd24c577ec27fc07084c9d7c1a7520
AlexB2008 <orangelightning08@gmail.com>
2024-01-21 00:14:54 -0700

    blog update

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,14 +9,9 @@ export default function() {
       }}>
       </div>
       <div className="right-column">
-        My Name is Alex Berry and I am learning to Code. 
-        This is my first website made with React with the help of the Code to Success Utah Summer Program. 
-        This is my second Website as well as my second summer in the Code to Success Program.
-        I am interested in Coding/Programming as well as working with the components of a computer. 
-        I have shown interest in computers for a couple years. 
-        I finished my Freshman year of High School and I am moving into my Softmore year where I will take more classes to learn about coding and computer components. 
-        I am very excited to see how I can improve over time and what projects I will make throughout my life. 
-        If you wish to see my HTML/CSS website made in my first year of the Code to Success Program, visit <a className="website-link" href="alexfantasticfries.neocities.org" target="_blank">alexfantasticfries.neocities.org</a>.
+        Made by Alex Berry and Quinn Heiner, The Nothing Podcast is a comidical podcast made for entertainment purposes only. 
+        Everyone has told us to have as much fun as we can in high school, so we made a podcast!
+        Our mission, and the purpose of this podcast, is too have fun and hang out as best friends while sharing our high school drama with the world.
       </div>
     </div>
   );
