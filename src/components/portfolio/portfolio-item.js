541ae6b95b4a7bff0c303958ec7f564f482ef5fa
AlexB2008 <orangelightning08@gmail.com>
2024-01-21 22:38:29 -0700

    portfolio detail update

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class PortfolioItem extends Component {
@@ -7,30 +7,34 @@ export default class PortfolioItem extends Component {
 
     this.state = {
       portfolioItemClass: ""
-    }
+    };
   }
 
-  handleMouseEnter()  {
-    this.setState({ portfolioItemClass: "image-blur" })
+  handleMouseEnter() {
+    this.setState({ portfolioItemClass: "image-blur" });
   }
 
-  handleMouseLeave()  {
-    this.setState({ portfolioItemClass: "" })
+  handleMouseLeave() {
+    this.setState({ portfolioItemClass: "" });
   }
 
   render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-      <div className="portfolio-item-wrapper"
-      onMouseEnter={() => this.handleMouseEnter()}
-      onMouseLeave={() => this.handleMouseLeave()}
-      >
+      <Link to={`/portfolio/${id}`}>
         <div
-          className={"portfolio-img-background " + this.state.portfolioItemClass}
-          style={{
-            backgroundImage: "url(" + thumb_image_url + ")"
-          }}
-        />
+          className="portfolio-item-wrapper"
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <div
+            className={
+              "portfolio-img-background " + this.state.portfolioItemClass
+            }
+            style={{
+              backgroundImage: "url(" + thumb_image_url + ")"
+            }}
+          />
 
           <div className="img-text-wrapper">
             <div className="logo-wrapper">
@@ -39,7 +43,8 @@ export default class PortfolioItem extends Component {
 
             <div className="subtitle">{description}</div>
           </div>
-      </div>
+        </div>
+      </Link>
     );
   }
 }
